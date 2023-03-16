1246d89d962fed234a00839b114ec71df85a5b60
Yagouch <yago.carballo@estudiantat.upc.edu>
2023-03-16 20:26:59 +0100

    first commit

---
+++ src/web.cpp
@@ -0,0 +1,10 @@
+#include <Arduino.h>
+
+String HTML = 
+"<!DOCTYPE html> "
+"<html> "
+"<body> "
+"<h1> Primera Pagina <\h1> "
+"<img src='https://static.nationalgeographic.es/files/styles/image_3200/public/75552.ngsversion.1422285553360.webp?w=1024&h=683' > "
+"</body> "
+"</html>";
