9b2a90c72a3f3f1524d064526d9a4703e2c4f850
Yagouch <yago.carballo@estudiantat.upc.edu>
2023-05-18 23:42:19 +0200

    images and Readme modified

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,56 @@
-/*
-  Practica SPI - RFID (Impresion por pantalla)
-*/
-
+#include <Arduino.h>
 #include <SPI.h>
 #include <MFRC522.h>
+#include <SD.h>
 
-#define RST_PIN	9    //Pin 9 para el reset del RC522
-#define SS_PIN	5   //Pin 10 para el SS (SDA) del RC522
-MFRC522 mfrc522(SS_PIN, RST_PIN); //Creamos el objeto para el RC522
+#define SS_RFID 10
+#define RST_PIN 9
+#define SS_SD 5
+
+MFRC522 rfid(SS_RFID, RST_PIN); // Creamos un objeto MFRC522
+File myFile;
 
 void setup() {
-	Serial.begin(115200); //Iniciamos la comunicación  serial
-	SPI.begin();        //Iniciamos el Bus SPI
-	mfrc522.PCD_Init(); // Iniciamos  el MFRC522
-	Serial.println("Lectura del UID");
+  Serial.begin(115200);
+
+  SPI.begin(); // Inicializamos el bus SPI
+  rfid.PCD_Init(); // Inicializamos el lector RFID
+
+  Serial.println("Lector RFID iniciado");
+
+  if (!SD.begin(SS_SD)) { // Inicializamos la tarjeta SD
+    Serial.println("Error al iniciar la tarjeta SD");
+    return;
+  }
+
+  Serial.println("Tarjeta SD iniciada");
 }
 
 void loop() {
-	// Revisamos si hay nuevas tarjetas  presentes
-	if ( mfrc522.PICC_IsNewCardPresent()) 
-        {  
-  		//Seleccionamos una tarjeta
-            if ( mfrc522.PICC_ReadCardSerial()) 
-            {
-                  // Enviamos serialemente su UID
-                  Serial.print("Card UID:");
-                  for (byte i = 0; i < mfrc522.uid.size; i++) {
-                          Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
-                          Serial.print(mfrc522.uid.uidByte[i], HEX);   
-                  } 
-                  Serial.println();
-                  // Terminamos la lectura de la tarjeta  actual
-                  mfrc522.PICC_HaltA();         
-            }      
-	}	
+  // Esperamos a que se detecte una etiqueta
+  if (!rfid.PICC_IsNewCardPresent() || !rfid.PICC_ReadCardSerial())
+    return;
+
+  // Obtenemos el UID de la etiqueta
+  String content = "";
+  for (byte i = 0; i < rfid.uid.size; i++) {
+    content.concat(String(rfid.uid.uidByte[i] < 0x10 ? "0" : ""));
+    content.concat(String(rfid.uid.uidByte[i], HEX));
+  }
+
+  // Escribimos el UID en la tarjeta SD
+  myFile = SD.open("lecturas.log", FILE_WRITE);
+  if (myFile) {
+    myFile.print(content);
+    myFile.println();
+    myFile.close();
+
+  }
+  else {
+    Serial.println("Error al abrir el archivo");
+  }
+
+  // Mostramos el UID por el monitor serial
+  Serial.println("Lectura RFID:");
+  Serial.println(content);
 }
